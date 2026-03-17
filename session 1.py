d35c25df84ba254e94325be8bf789d350eeaf2b9
lukasbaumgartner <lukasfbaumgartner@gmail.com>
2026-03-17 16:42:01 +0100

    first commit

---
+++ session 1.py
@@ -0,0 +1,10 @@
+class Point:
+    def __init__(self, x, y):
+        """
+        Initialize the point
+        :param x: x-coordinate
+        :param y: y-coordinate
+        """
+        self.x = x
+        self.y = y
+    # Use Capital first letter (custom or prexisting class)
